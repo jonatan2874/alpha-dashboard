676adc9b418edf0ba9cdabf494634a844f29fca7
jonatan2874@gmail.com <jonatan2874@gmail.com>
2024-01-17 20:55:04 -0500

    componente error 404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Error404 = () => {
   return (
-    <div>Error404</div>
+    <div className="min-h-screen flex items-start justify-center bg-secondary-900 text-gray-300">
+      <div className="bg-secondary-100 p-8 rounded-xl shadow-xl" >
+        <h1 className="text-3xl text-center font-bold tracking-[5px] text-white mb-8 w-auto lg:w-[450px]">Error 404</h1>
+      </div>
+    </div>
   )
 }
 
